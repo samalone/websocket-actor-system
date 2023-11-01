6a6d52d95983f601a62729f7e3b0647a845b2a54
Stuart A. Malone <samalone@llamagraphics.com>
2023-11-01 09:12:11 -0400

    Rename NaiveLogging and fix logger label.

--- Sources/WebSocketActors/Logging.swift
+++ Sources/WebSocketActors/Logging.swift
@@ -2,13 +2,13 @@
 See LICENSE folder for this sample’s licensing information.
 
 Abstract:
-Simplistic "fake" logging infrastructure, just so we can easily print and verify output from a simulator running app.
+Wrappers around Logging package to add metadata automatically.
 */
 
 import Foundation
 import Logging
 
-let logger = Logger(label: "com.llamagraphics.WebSocketActorSystem")
+let logger = Logger(label: "com.llamagraphics.WebSocketActors")
 
 public func debug(_ category: String, _ message: String, file: String = #fileID, line: Int = #line, function: String = #function) {
     logger.debug("[\(category)][\(file):\(line)](\(function)) \(message)")
