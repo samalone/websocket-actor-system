ec6aead4c7ccdad5a55bc625d6fab51a57823476
Stuart A. Malone <samalone@llamagraphics.com>
2023-12-08 10:24:14 -0500

    Improve error handling when there is no matching Node ID Fixes #9

--- Sources/WebSocketActors/Continuation.swift
+++ Sources/WebSocketActors/Continuation.swift
@@ -39,3 +39,47 @@ import Foundation
         try await withUnsafeThrowingContinuation(function: function, body)
     }
 #endif
+
+/// A Continuation with a timeout. The continuation will either resume with
+/// the value passed to `resume(returning:)`, or with the provided
+/// error after the timeout expires.
+actor TimedContinuation<T> {
+    var continuation: Continuation<T, Error>?
+    var timeoutTask: Task<Void, Error>?
+
+    init(continuation: Continuation<T, Error>,
+         error timeoutError: Error,
+         timeout: Duration,
+         tolerance: Duration? = nil) async {
+        self.continuation = continuation
+        timeoutTask = Task {
+            do {
+                try await Task.sleep(for: timeout, tolerance: tolerance)
+                self.resume(throwing: timeoutError)
+            }
+            catch {
+                self.resume(throwing: error)
+            }
+        }
+    }
+
+    private func cancelTimeout() {
+        guard let timeoutTask else { return }
+        timeoutTask.cancel()
+        self.timeoutTask = nil
+    }
+
+    func resume(throwing error: Error) {
+        guard let continuation else { return }
+        continuation.resume(throwing: error)
+        self.continuation = nil
+        cancelTimeout()
+    }
+
+    func resume(returning value: T) {
+        guard let continuation else { return }
+        continuation.resume(returning: value)
+        self.continuation = nil
+        cancelTimeout()
+    }
+}
